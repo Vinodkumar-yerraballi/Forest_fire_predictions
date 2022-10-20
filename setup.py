6948f1aec024f225162ea05b1f1c083d64a651ee
Vinodkumar-yerraballi <vinodkumar9676150@gmail.com>
2022-10-20 13:35:43 +0530

    update my files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="src",
+    version="0.0.1",
+    description="its a Forest Fire Prediction", 
+    author="Vinod", 
+    packages=find_packages(),
+    license="MIT"
+)
